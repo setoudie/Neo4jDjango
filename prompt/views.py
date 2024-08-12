082d6cc9fcbb29fce24674d48510ea926bc9350f
setoudie <setoudie@gmail.com>
2024-08-12 23:38:20 +0000

    urls test

--- prompt/views.py
+++ prompt/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# My first test view in django
+def index(request):
+    return HttpResponse("<h1>Ceci est un test</h1>")
+
+# Another test view
+def test(request):
+    return HttpResponse('<li> Test 1 </li>')
